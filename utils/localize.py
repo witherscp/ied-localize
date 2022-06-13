3e9216d1eb19f86a3df47270e3ba71451e6ebcf1
Price Withers <price.withers@nih.gov>
2022-06-13 12:47:42 -0400

    Add n_steps argument to find_denom_range

--- utils/localize.py
+++ utils/localize.py
@@ -8,7 +8,8 @@ from .constants import *
 
 
 def lead_geodesic(Subj, elec_idxs, parc_idxs, lags, hemis, cluster_hemi, 
-                  minGeo, maxGeo, minBL, maxBL, only_geo=False, only_wm=False):
+                  minGeo, maxGeo, minBL, maxBL, only_geo=False, only_wm=False,
+                  n_steps=20):
     """Localize the sources of a sequence based on the assumption that the lead
     electrode receives signal via geodesic spread. Returns a list of unique 
     parcel numbers.
@@ -30,6 +31,9 @@ def lead_geodesic(Subj, elec_idxs, parc_idxs, lags, hemis, cluster_hemi,
             propagation. Defaults to False.
         only_wm (bool, optional): require all white matter propagation 
             (immediately returns []). Defaults to False.
+        n_steps (int, optional): number of steps used in linspace; a larger 
+            number will increase max/min range but slow down algorithm. 
+            Defaults to 20.
 
     Returns:
         list: list of unique source parcel numbers (if [], no parcels explain 
@@ -82,7 +86,7 @@ def lead_geodesic(Subj, elec_idxs, parc_idxs, lags, hemis, cluster_hemi,
         
         # iterate through parcel combos
         for f_parc_idxs_combo in product(*f_parc_idxs):
-          
+        
             # fill arrays
             parc_min_bl = minBL[:,f_parc_idxs_combo]
             parc_max_bl = maxBL[:,f_parc_idxs_combo]
@@ -92,7 +96,8 @@ def lead_geodesic(Subj, elec_idxs, parc_idxs, lags, hemis, cluster_hemi,
                                                               parc_max_bl,
                                                               MIN_WM_VEL,
                                                               MAX_WM_VEL,
-                                                              lags)
+                                                              lags,
+                                                              n_steps=n_steps)
             
             # initialize n_node x n_elec arrays to store denominators
             min_denom = np.full((N_NODES,n_followers), np.NaN)
@@ -116,12 +121,12 @@ def lead_geodesic(Subj, elec_idxs, parc_idxs, lags, hemis, cluster_hemi,
           
             # create matrix of shape (N_NODES,n_elecs,v/combo_v,min/max)
             master_v = np.stack((np.stack((min_v_geo,combo_min_v_geo), 
-                                            axis=-1), 
+                                           axis=-1), 
                                 np.stack((max_v_geo,combo_max_v_geo), 
-                                            axis=-1)), 
+                                          axis=-1)), 
                                 axis=-1)
           
-            # shuffle combinations of v/combo_v #######
+            # shuffle combinations of v/combo_v
             # shuffled array has shape (N_NODES,2**n_elecs,n_elecs,min/max)
             # each index in the second dimension is a different combination
             # of v/combo_v
@@ -150,7 +155,7 @@ def lead_geodesic(Subj, elec_idxs, parc_idxs, lags, hemis, cluster_hemi,
         return [source for source in source_parcs if source != 0]
     
 def lead_wm(elec_idxs, parc_idxs, lags, parc_minGeo, parc_maxGeo, minBL, 
-            maxBL, only_geo=False, only_wm=False):
+            maxBL, only_geo=False, only_wm=False, n_steps=10):
     """Localize the sources of a sequence based on the assumption that the lead
     electrode receives signal via geodesic spread. Returns a list of unique 
     parcel numbers.
@@ -169,6 +174,9 @@ def lead_wm(elec_idxs, parc_idxs, lags, parc_minGeo, parc_maxGeo, minBL,
             (immediately returns []). Defaults to False.
         only_wm (bool, optional): require followers to use white matter 
             propagation. Defaults to False.
+        n_steps (int, optional): number of steps used in linspace; a larger 
+            number will increase max/min range but slow down algorithm. 
+            Defaults to 10.
 
     Returns:
         list: list of unique source parcel numbers (if [], then no parcels 
@@ -195,7 +203,8 @@ def lead_wm(elec_idxs, parc_idxs, lags, parc_minGeo, parc_maxGeo, minBL,
                                                 MIN_GEO_VEL,
                                                 MAX_GEO_VEL,
                                                 lags,
-                                                fixed_velocity=True)
+                                                fixed_velocity=True,
+                                                n_steps=n_steps)
         
         # num of steps used for testing velocities and distances
         n_steps = min_denom.shape[0]
@@ -225,7 +234,8 @@ def lead_wm(elec_idxs, parc_idxs, lags, parc_minGeo, parc_maxGeo, minBL,
                                                               parc_max_bl,
                                                               MIN_WM_VEL,
                                                               MAX_WM_VEL,
-                                                              lags)
+                                                              lags,
+                                                              n_steps=n_steps)
             
             # compute min/max velocity wm using equation from methods section
             min_v_wm = aveBL[:,l_parc_idx][:,np.newaxis] / parc_max_denom
@@ -287,15 +297,31 @@ def lead_wm(elec_idxs, parc_idxs, lags, parc_minGeo, parc_maxGeo, minBL,
         return [parc_idx+1 for parc_idx in source_parcs]
     
 def find_denom_range(min_dist, max_dist, min_vel, max_vel, lags, 
-                     fixed_velocity=False):
-    
-    n_steps = 50 # step size of interval range
+                     fixed_velocity=False, n_steps=50):
+    """Find min/max range of localization denominator.
+
+    Args:
+        min_dist (np.array): minimum distance from source to followers
+        max_dist (np.array): maximum distance from source to followers
+        min_vel (float): minimum velocity (geodesic or wm)
+        max_vel (float): maximum velocity (geodesic or wm)
+        lags (np.array): array of lag times to followers
+        fixed_velocity (bool, optional): followers must receive signal at the 
+            same velocity (used for lead wm/ follower geodesic). Defaults to 
+            False.
+        n_steps (int, optional): number of steps used in linspace; a larger 
+            number will increase max/min range but slow down algorithm. 
+            Defaults to 50.
+
+    Returns:
+        tuple: minimum denominator (np.array), maximum denominator (np.array)
+    """
     
     # shape (n_steps,n_regions,n_elecs)
-    dist_range = np.linspace(min_dist,max_dist,n_steps)
+    dist_range = np.linspace(min_dist, max_dist, n_steps)
     
     # shape (n_steps)
-    vel_range = np.linspace(min_vel,max_vel,n_steps)
+    vel_range = np.linspace(min_vel, max_vel, n_steps)
     
     # calculate denominator with equation (dist/vel - lags)
     # shape (n_steps,n_steps,n_regions,n_elecs)
